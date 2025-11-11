ae8d66d62ace83ed259404808960d1abcb687277
HRLopezP <hrlp843@gmail.com>
2025-11-11 06:07:48 +0000

    Se realizó el formulario y backend del registro

--- src/api/models.py
+++ src/api/models.py
@@ -7,19 +7,25 @@ from typing import Optional
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id_user: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(30), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     fullname: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
-    rol: Mapped[str] = mapped_column(String(20), nullable = False, default= "usuario")
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now (timezone.utc), nullable=False)
+    rol: Mapped[str] = mapped_column(
+        String(20), nullable=False, default="usuario")
+    created_at: Mapped[datetime] = mapped_column(DateTime(
+        timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(
+        timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    salt: Mapped[str] = mapped_column(String(50), nullable = False)
-    profile: Mapped[str] = mapped_column(String(255), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable = True)
-
+    salt: Mapped[str] = mapped_column(String(50), nullable=False)
+    profile: Mapped[str] = mapped_column(String(
+        255), nullable=False, default=lambda context: "https://ui-avatars.com/api/?name={}&size=128&background=random&rounded=true".format(context.get_current_parameters()['username']))
+    is_active: Mapped[bool] = mapped_column(
+        Boolean(), nullable=True, default=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
--- src/api/routes.py
+++ src/api/routes.py
@@ -10,14 +10,14 @@ from base64 import b64encode
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from datetime import timedelta
-import cloudinary.uploader as uploader
 
- 
+
 api = Blueprint('api', __name__)
 
- 
+
 # Allow CORS requests to this API
 CORS(api)
+
 
 @api.route("/health-check", methods=["GET"])
 def health_check():
@@ -26,43 +26,36 @@ def health_check():
 
 @api.route("/register", methods=["POST"])
 def register_user():
-    data_form = request.form
 
-    data = {
-        "email": data_form.get("email"),
-        "password": data_form.get("password"),
-        "username": data_form.get("username"),
-        "fullname": data_form.get("fullname"),
-        "is_active": True
-    }
+    data = request.get_json(silent=True)
 
-    if not data["email"] or not data["username"] or not data["password"]:
+    if data is None:
+        return jsonify({"message": "Invalid JSON or no data provided"}), 400
+
+    email = data.get("email")
+    password = data.get("password")
+    username = data.get("username")
+    fullname = data.get("fullname")
+
+    if not email or not username or not password:
         return jsonify({"message": "Email, username and password are required"}), 400
-    if not val_email(data["email"]):
+    if not val_email(email):
         return jsonify({"message": "Email is invalid,"}), 400
-    if not val_password(data["password"]):
+    if not val_password(password):
         return jsonify({"message": "Password is invalid. Requires 8+ chars, upper/lower case, number, special char, and no 3 consecutive numbers."}), 400
-    if User.query.filter_by(email=data["email"]).first():
+    if User.query.filter_by(email=email).first():
         return jsonify({"message": "The email address is already registered."}), 409
-    if User.query.filter_by(username=data["username"]).first():
+    if User.query.filter_by(username=username).first():
         return jsonify({"message": "The username is already registered"}), 409
- 
+
     salt = b64encode(os.urandom(16)).decode("utf-8")
     hashed_password = generate_password_hash(f"{data['password']}{salt}")
 
-    name_url = data["username"]
-
-    photo_url = "https://ui-avatars.com/api/?name={}&size=128&background=random&rounded=true".format(
-        name_url
-    )
-
     new_user = User(
-        email=data["email"],
+        email=email,
         password=hashed_password,
-        fullname=data["fullname"],
-        username=data["username"],
-        profile=photo_url,
-        is_active=data["is_active"],
+        fullname=fullname,
+        username=username,
         salt=salt,
     )
 
@@ -70,7 +63,10 @@ def register_user():
 
     try:
         db.session.commit()
-        return jsonify({"message": "user created succesfuly"}), 201
+        return jsonify({
+            "message": "user created succesfuly",
+            "user": new_user.serialize()
+        }), 201
     except Exception as error:
         db.session.rollback()
         return jsonify({"message": "Error creating user", "Error": f"{error.args}"}), 500
