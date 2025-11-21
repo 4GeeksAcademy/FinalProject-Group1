ddcaf7e13b998c219cf596031b5c03d8e502cda2
Carlos Meneses <carlosocharly1@hotmail.com>
2025-11-21 22:35:12 +0000

    add fix in layout

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -7,11 +7,21 @@ import Register from "./Register"
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
    return (
-        <ScrollToTop>
-            <Navbar />
-            <Outlet />
-            <Footer />
-            
-        </ScrollToTop>
-    )
+      <div style={{ 
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between"
+}}>
+            <ScrollToTop>
+                <Navbar />
+
+                <main style={{ flex: 1 }}>
+                    <Outlet />
+                </main>
+
+                <Footer />
+            </ScrollToTop>
+        </div>
+    );
 }
